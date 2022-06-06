7072d1ab1f49cc6c96f716d7c2b84fe1fd502973
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2022-06-05 23:17:46 -0400

    cleaned and updated

---
+++ README.md
@@ -0,0 +1,11 @@
+# Drumming Notation Study: Backend
+
+This repo contains the back-end for the Drumming Notation Study as part of my thesis, "Improving music education using biofeedback-based cognitive tutoring." This backend is developed in Python using Flask.
+
+## Instructions to run
+1. Create a virtual environment with `python3 -m venv env`
+2. Activate the virtual environment with `source env/bin/activate`
+3. Install the packages required with `pip install -r requirements.txt`
+4. Run `deactivate`
+5. Activate the virtual environment with `source env/bin/activate`
+6. Start the backend with `flask run`
--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Name: Jordan Sanz
+# Title: Drumming Notation Study main app
+# Purpose: Holds the main app for the backend
 
-# [START gae_flex_quickstart]
 import math
 from flask import Flask, app, request
 from flask_cors import CORS
@@ -22,54 +11,51 @@ import numpy as np
 from werkzeug.utils import secure_filename
 import json
 
+# configuration
 UPLOAD_FOLDER = './videos'
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+# import models
 face_model = "faceboxes"
 landmark_model = "mobilenet"
 au_model = "rf"
 emotion_model = "resmasknet"
-#logging.warning("Right before detector load")
 detector = Detector(face_model = face_model, landmark_model = landmark_model, emotion_model = emotion_model)
-#logging.warning('LOADED. READY TO LISTEN.')
 print("LOADED AND LISTENING")
 
+# constants
 TESTING_AVERAGE = .44
 IS_RENDER_COM = False
-if IS_RENDER_COM:
-    UPLOAD_FOLDER = '/opt/render/project/src/videos'
-else:
-    UPLOAD_FOLDER = './videos'
+UPLOAD_FOLDER = './videos'
 
+# create upload folder on deployed server
 if not os.path.exists(UPLOAD_FOLDER):
-    print('making upload folder')
-    # logging.warning('having to make upload folder...')
     os.mkdir(UPLOAD_FOLDER)
 
 
+# default
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+
+# post route for video upload
 @app.route('/video', methods=['POST'])
 def read_video():
-    print("read video path hit")
+
+    # get video
     video = request.files['video']
     filename = secure_filename(video.filename)
-    print(filename, 'filename')
-    print('directory: ', os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    print('after write to os')
-    print('current directory: ', os.path.curdir)
-
     video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    print("file exists?", os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+
+    # create video prediction
     video_prediction = detector.detect_video(os.path.join(app.config['UPLOAD_FOLDER'], filename), skip_frames=98, verbose=True)
-    print("after video prediction")
-    print('emotions: ', video_prediction.emotions())
+
+    # get emotion json
     emotions = {
     "happiness": video_prediction["happiness"].to_json(orient='values'),
     "anger": video_prediction["anger"].to_json(orient='values'), 
@@ -79,8 +65,8 @@ def read_video():
     "surprise": video_prediction["surprise"].to_json(orient='values'),
     "neutral": video_prediction["neutral"].to_json(orient='values'),
     }
-    print(emotions)
 
+    # find means of each emotion
     anger = video_prediction["anger"].mean()
     sadness = video_prediction["sadness"].mean()
     fear = video_prediction["fear"].mean()
@@ -89,6 +75,7 @@ def read_video():
     neutral = video_prediction["neutral"].mean()
     surprise = video_prediction["surprise"].mean()
 
+    # get means in json form with raw dataframe
     means = {
         "anger": np.float64(anger),
         "sadness": np.float64(sadness),
@@ -100,17 +87,12 @@ def read_video():
         "dataframe": emotions,
     }
 
-    print("means: ")
-    print(means)
-    
+    # return calculated versions back
     return json.dumps(means, indent = 4)
 
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app.
     HOST = '0.0.0.0'
     PORT = 8080
     app.run(host=HOST, port=PORT, debug=True)
-# [END gae_flex_quickstart]
